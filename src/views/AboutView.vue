cc38187dfb79b0e2c8bb724e014772775ca38975
kanzakisuemi <jsuemikanzaki@gmail.com>
2024-06-04 01:13:48 -0300

    adicionando o pedro

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
+  <div class="">
     <h1>Quem é Pedro?</h1>
-    <img src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExeW95OWhwZXNmejBzeDBjeW14cGJ2emJ5Z2t1M2hncGtrb2pscjMwNyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ZJPSFNLmADueHvzoZ8/giphy.gif" alt="">
+    <div>
+      <h3>Escute Pedro no Youtube</h3>
+      <iframe width="420" height="315" src="https://www.youtube.com/embed/F2YpXC1itEE"></iframe>
+    </div>
+    <div class="">
+      <h3>Escute Pedro no Spotify</h3>
+      <p style="color: grey;">scaneie o código de barras pelo app do spotify</p>
+      <img 
+        src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExeW95OWhwZXNmejBzeDBjeW14cGJ2emJ5Z2t1M2hncGtrb2pscjMwNyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ZJPSFNLmADueHvzoZ8/giphy.gif"
+        alt="" 
+        width="20%"
+        class="d-block mx-auto"
+      >
+      <img src="../assets/spotify-pedro-barcode.jpeg" alt="" width="20%" class="d-block mx-auto">  
+    </div>
   </div>
 </template>
